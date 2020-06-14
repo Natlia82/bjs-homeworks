86d26878526816d10b4e32c8cd33612580c2ea88
Natalia <nata@host>
2020-06-14 13:43:26 +0500

    задание 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,46 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+"use strict";
+
+function getResult(a, b, c) {
+    let D = (Math.pow(b, 2)) - 4 * a * c;
+    let rezult = [];
+    if (D > 0) {
+        rezult[0] = (-b + (Math.sqrt(D))) / 2 * a;
+        rezult[1] = (-b - (Math.sqrt(D))) / 2 * a;
+    } else if (D == 0) {
+        rezult[0] = (-b + (Math.sqrt(D))) / 2 * a;
+    }
+
+
+    return rezult;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+
+function getAverageMark(marks) {
+    let summ = 0;
+    let averageMark;
+    if (marks.length > 5) {
+        console.log("оценок больше 5");
+        marks = marks.slice(0, 5);
+    }
+    if (marks.length != 0) {
+        for (const iterator of marks) {
+            summ += iterator;
+        }
+        averageMark = (summ / marks.length);
+    } else {
+        averageMark = 0;
+    }
+
+    return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+
+function askDrink(name, dateOfBirthday) {
+    let dat = new Date();
+    let result;
+    let age = (dat.getFullYear() - dateOfBirthday.getFullYear());
+    let month = (dat.getMonth() > dateOfBirthday.getMonth()) ? age : age = age - 1;
+    (age > 18) ? result = `Не желаете ли олд-фэшн, ${name} ?`: result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+
+    return result;
 }
