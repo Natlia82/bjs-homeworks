867d878751dcfdb102610614fd1c122156199ee4
Natalia <nata@host>
2020-06-24 13:51:55 +0500

    Задача 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,42 @@
 //String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {
+    let str = this.toLowerCase().replace(/\s/g, "");
+    let len = str.length;
+
+    for (i = 0; i < len / 2; ++i) {
+        if (str[i] != str[len - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let summ = 0;
+    let average;
+    let roundedAverage;
+
+    if (marks.length == 0) {
+        return 0;
+    }
+
+    for (value of marks) {
+        summ += value;
+    }
+    average = summ / marks.length;
+    roundedAverage = Math.round(average);
+
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now();
+    birthday = Number(birthday);
+    let diff = now - birthday;
+    let age = diff / 31536000000;
+
+    if (age > 18) {
+        return true;
+    } else return false;
+
 }
